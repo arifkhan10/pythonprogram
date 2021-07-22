05bf3bc1bd46049e257bc3eae078b21e1d630bd4
arifkhan10 <barfikhan78621@gmail.com>
2021-07-22 12:26:28 +0530

    donetask3

---
+++ task3.py
@@ -0,0 +1,22 @@
+n1=int(input("enter the number"))
+
+even=[]
+
+odd=[]
+
+def myFind(n1):
+
+    if (n1 % 2==0):
+
+        even.append(n1) 
+    else:
+        odd.append(n2)   
+
+myFind(n1)
+print(even)
+print(odd)
+
+
+
+         
+
