78b33899a46b0d8517b35a5d0d3f99238e4b442f
arifkhan10 <barfikhan78621@gmail.com>
2021-07-23 11:29:26 +0530

    module

---
+++ Calculatormodule.py
@@ -0,0 +1,9 @@
+def sumTwo(a,b):
+    return a+b
+def Subtwo(a,b):
+    return a-b
+def multwo(a,b):
+    return a*b
+def divtwo(a,b):
+    return a//b 
+    
---
+++ calculator.py
@@ -0,0 +1,7 @@
+import Calculatormodule as y
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+print(y.sumTwo(num1,num2))
+print(y.Subtwo(num1,num2))
+print(y.multwo(num1,num2))
+print(y.divtwo(num1,num2))
