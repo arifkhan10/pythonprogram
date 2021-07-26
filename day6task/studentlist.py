930cceabe6a753c02ddbffa75f383ae80a04049d
arifkhan10 <barfikhan78621@gmail.com>
2021-07-26 17:01:16 +0530

    day6

--- day6task/studentlist.py
+++ day6task/studentlist.py
@@ -1,22 +1,12 @@
-#List of objects
-
-
+#list of object
 class Students:
-
-    def __int__(self,name,rollno,admin):
+    def __init__(self,name,rollno,admin):
         self.myname=name
-
         self.myrollno=rollno
-
         self.myadmin=admin 
 obj=[]
-
 obj.append(Students("Arif",12,234)) 
-
 obj.append(Students("khan",13,231)) 
-
 obj.append(Students("Gulshan",14,236))
-
 print(obj[0].myname)
-
-print(obj[1].myrollno)
+print(obj[0].myrollno)
