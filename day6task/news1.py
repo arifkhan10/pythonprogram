3aecd47fd2bf5f4bb87fe84641e5b8e8ec179bd1
arifkhan10 <barfikhan78621@gmail.com>
2021-07-28 08:09:45 +0530

    day7work

--- day6task/news1.py
+++ day6task/news1.py
@@ -1,4 +1,20 @@
+import json
 import requests
 data=requests.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=5b00428da5f24a4dae3f2558212dd2d8")
+ExtractedData=data.json()
+#print(ExtractedData)
+#print(ExtractedData["status"])
 #print(data)
-print(data.text)
+#print(data.text)
+#print(data.text)
+articles=ExtractedData["articles"]
+vowel="aeiou"
+
+for i in articles:
+    title=i["title"]
+count={}.fromkeys(vowel,0)
+print(title.lower())
+for x in title.lower():
+    if x in count:
+        count[x]=count[x]+1
+print(count)        
